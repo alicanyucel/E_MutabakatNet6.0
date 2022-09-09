fdecde29cdbc58b1dfc0cd23bfee17631721ff77
Ali Can Yücel <32604562+alicanyucel@users.noreply.github.com>
2022-09-09 18:20:42 +0300

    Generic Repository kodlandı.

---
+++ É_Mutabakat.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using É_Mutabakat.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace É_Mutabakat.Core.DataAccess
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        List<T> GetList(Expression<Func<T, bool>> filter =null);
+        T Get(Expression<Func<T, bool>> filter );
+
+
+    }
+}
