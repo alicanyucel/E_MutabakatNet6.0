05890cb3ef28aeecb68e68f805be239e44a57934
Ali Can Yücel <32604562+alicanyucel@users.noreply.github.com>
2022-09-09 21:16:34 +0300

    Devam

---
+++ E_Mutabakat.DataAccess/Concrete/EntityFrameWork/EfBabsReconcliationDetailDal.cs
@@ -0,0 +1,16 @@
+﻿using É_Mutabakat.Core.DataAccess.EntityFramework;
+using E_Mutabakat.DataAccess.Abstract;
+using E_Mutabakat.DataAccess.Concrete.EntityFrameWork.Context;
+using E_Mutabakat.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Mutabakat.DataAccess.Concrete.EntityFrameWork
+{
+    public class EfBabsReconcliationDetailDal:EfEntityFrameworkBase<BaBsReconcilitionsDetail,ContextDb>,IBabsReconciliationDetailDal
+    {
+    }
+}
