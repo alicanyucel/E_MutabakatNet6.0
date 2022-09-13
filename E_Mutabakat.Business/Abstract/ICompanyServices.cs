ed70d2301833c8b9c10348790470440c87fb2102
Ali Can Yücel <32604562+alicanyucel@users.noreply.github.com>
2022-09-13 15:37:51 +0300

    Business Katmanı Dependency Injection Ok

---
+++ E_Mutabakat.Business/Abstract/ICompanyServices.cs
@@ -0,0 +1,14 @@
+﻿using E_Mutabakat.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Mutabakat.Business.Abstract
+{
+    public interface ICompanyServices
+    {
+        List<Company> GetList();
+    }
+}
---
+++ E_Mutabakat.Business/Concrete/CompanyManager.cs
@@ -0,0 +1,31 @@
+﻿using E_Mutabakat.Business.Abstract;
+using E_Mutabakat.DataAccess.Abstract;
+using E_Mutabakat.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Mutabakat.Business.Concrete
+{
+    public class CompanyManager : ICompanyServices
+    {
+        /*
+        KULLANICI YETKİLERİ
+        validation
+        Transaction
+        loglama için business katmanı kullanıyoruz
+        */
+        private readonly ICompanyDal _companyDal;
+        public CompanyManager(ICompanyDal companyDal)
+        {
+            _companyDal = companyDal;
+        }
+
+        public List<Company> GetList()
+        {
+            return _companyDal.GetList();
+        }
+    }
+}
